fc36a0af05f15e666bb35b40e009242f0eff0e77
Дід Alik <didalik.aim@gmail.com>
2023-10-18 22:15:24 +0000

    Use history.log

--- cfw/src/shared/user-url.mjs
+++ cfw/src/shared/user-url.mjs
@@ -2,3 +2,7 @@ import { setup, } from './util.mjs' // {{{1
 
 setup() // {{{1
 
+window.addEventListener('message', e => {
+  e.data.forEach(e => console.log(e))
+})
+window.opener.postMessage('user-url started', window.opener.location)
