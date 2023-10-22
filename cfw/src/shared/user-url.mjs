f18f399278ffbbfa9464440381624db2e8474bf0
Дід Alik <didalik.aim@gmail.com>
2023-10-22 00:23:12 +0000

    Mark me

--- cfw/src/shared/user-url.mjs
+++ cfw/src/shared/user-url.mjs
@@ -1,15 +1,12 @@
-import { markAgent, setup, } from './util.mjs' // {{{1
+import { markup, setup, } from './util.mjs' // {{{1
 
-let agent
+let guestId = +window.location.pathname.split('/')[3]
 
 window.addEventListener('message', e => { // {{{1
-  console.log(e.data, typeof e.data[0])
+  //console.log(e.data, typeof e.data[0])
   if (typeof e.data[0] == 'string') {
-    if (!agent) {
-      agent = markAgent(e.data)
-    }
-    return;
+    return markup(e.data);
   }
-  setup({ lat: e.data[0], lng: e.data[1] }, e.data[2])
+  setup({ lat: e.data[0], lng: e.data[1] }, guestId)
 })
-window.opener.postMessage('user-url started', window.opener.location)
+window.opener.postMessage('user-url started, guestId ' + guestId, window.opener.location)
