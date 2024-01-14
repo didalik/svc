e8b27344f28056a55f791d312741bd67d0fbace4
Дід Alik <didalik.aim@gmail.com>
2024-01-14 22:38:51 +0000

    Add function start_local_dev

--- lib/util.sh
+++ lib/util.sh
@@ -13,6 +13,29 @@ function cc () { # clone our repo, checkout its main branch {{{1
   git checkout main > /dev/null 2>&1
 }
 
+function start_local_dev () { # for svc $1, log to $LOCALDEV_LOG, $! >> .pids2kill {{{1
+  local svc_name=$1
+  local svc_count=$2
+  local svc_dir=../../../svc/${svc_name}
+  local dev_fifo=${svc_dir}/dev.fifo
+  local dev_script=${svc_dir}/${svc_name}-dev.sh
+  local grep_pattern='wrangler-dist/cli.js dev'
+
+  echo "- $0 checking local svc ${svc_name}..." >> $LOCALDEV_LOG
+  if [ $(ps -ef|grep "${svc_name}-dev.sh"|wc -l) -eq 1 ]; then
+    echo "- $0 starting local svc ${svc_name}..." >> $LOCALDEV_LOG
+    $dev_script >> $LOCALDEV_LOG &
+    echo $! >> .pids2kill
+    echo $svc_name > $dev_fifo
+    while [ $(ps -ef|grep "$grep_pattern"|wc -l) -lt $svc_count ]; do
+      echo -e "\t- $dev_script starting wrangler dev..." >> $LOCALDEV_LOG
+      sleep 1
+    done
+    sleep 1
+  fi
+  echo "- $0 local svc ${svc_name} is ON." >> $LOCALDEV_LOG
+}
+
 function usage () { # {{{1
   cat << USAGE
 
